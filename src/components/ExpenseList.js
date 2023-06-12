2b0614525839473731f1577815d431b03b245973
Brandon Islas <104156453+SlimNorris@users.noreply.github.com>
2023-06-12 20:25:44 -0300

    Update ExpenseList.js

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,34 +1,17 @@
 import React, { useContext } from 'react';
-import ExpenseItem from './ExpenseItem';
 import { AppContext } from '../context/AppContext';
 
-const ExpenseList = () => {
-  const { expenses, currency, dispatch } = useContext(AppContext);
+const ExpenseTotal = () => {
+  const { expenses, currency } = useContext(AppContext);
+  const totalExpenses = expenses.reduce((total, item) => {
+    return (total += item.cost);
+  }, 0);
 
   return (
-    <table className='table'>
-      <thead className="thead-light">
-        <tr>
-          <th scope="col">Department</th>
-          <th scope="col">Allocated Budget</th>
-          <th scope="col">Increase by 10</th>
-          <th scope="col">Decrease by 10</th>
-          <th scope="col">Delete</th>
-        </tr>
-      </thead>
-      <tbody>
-        {expenses.map((expense) => (
-          <ExpenseItem
-            id={expense.id}
-            key={expense.id}
-            name={expense.name}
-            cost={expense.cost}
-            currency={currency}
-          />
-        ))}
-      </tbody>
-    </table>
+    <div className='alert alert-primary'>
+      <span>Spent so far: {currency}{totalExpenses}</span>
+    </div>
   );
 };
 
-export default ExpenseList;
+export default ExpenseTotal;
