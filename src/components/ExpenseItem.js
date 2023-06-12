2789c0748c40c4de2e92d6d833386f8f9c01bfe2
Brandon Islas <104156453+SlimNorris@users.noreply.github.com>
2023-06-12 20:25:20 -0300

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,37 +2,67 @@ import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
 
-const ExpenseItem = (props) => {
-    const { dispatch } = useContext(AppContext);
+const ExpenseItem = ({ id, name, cost, currency }) => {
+  const { dispatch } = useContext(AppContext);
 
-    const handleDeleteExpense = () => {
-        dispatch({
-            type: 'DELETE_EXPENSE',
-            payload: props.id,
-        });
+  const handleDeleteExpense = () => {
+    dispatch({
+      type: 'DELETE_EXPENSE',
+      payload: id,
+    });
+  };
+
+  const increaseAllocation = (name) => {
+    const expense = {
+      name: name,
+      cost: 10,
     };
 
-    const increaseAllocation = (name) => {
-        const expense = {
-            name: name,
-            cost: 10,
-        };
+    dispatch({
+      type: 'ADD_EXPENSE',
+      payload: expense,
+    });
+  };
 
-        dispatch({
-            type: 'ADD_EXPENSE',
-            payload: expense
-        });
+  const decreaseAllocation = (name) => {
+    const expense = {
+      name: name,
+      cost: -10,
+    };
 
-    }
+    dispatch({
+      type: 'ADD_EXPENSE',
+      payload: expense,
+    });
+  };
 
-    return (
-        <tr>
-        <td>{props.name}</td>
-        <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
-        </tr>
-    );
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{currency} {cost}</td>
+      <td>
+        <button
+          className="btn btn-success rounded-circle d-flex align-items-center justify-content-center"
+          onClick={() => increaseAllocation(name)}
+          style={{ width: '30px', height: '30px', fontSize: '30px', fontFamily: 'Arial' }}
+        >
+          +
+        </button>
+      </td>
+      <td>
+        <button
+          className="btn btn-danger rounded-circle d-flex align-items-center justify-content-center"
+          onClick={() => decreaseAllocation(name)}
+          style={{ width: '30px', height: '30px', fontSize: '30px', fontFamily: 'Arial' }}
+        >
+          -
+        </button>
+      </td>
+      <td>
+        <TiDelete size='1.5em' onClick={handleDeleteExpense} />
+      </td>
+    </tr>
+  );
 };
 
 export default ExpenseItem;
